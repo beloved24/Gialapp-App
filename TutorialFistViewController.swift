db47095d65219677fd1e2fe6318df79313e05ac8
Andrew Amato <andrew.amato95@gmail.com>
2017-06-05 18:06:04 +0200

    riordinato

--- TutorialFistViewController.swift
+++ TutorialFistViewController.swift
@@ -13,28 +13,30 @@ class TutorialFistViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationBar()
-        
-     
-
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.isNavigationBarHidden = false
-//        navigationController?.navigationBar.barTintColor = UIColor(red: 32, green: 99, blue: 124, alpha: 0)
-        
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+      
     }
+    
+  
+}
+
+
+
+extension TutorialFistViewController {
     
     func setNavigationBar () {
         let navBarImageTitle = UIImageView(image: #imageLiteral(resourceName: "megaride"))
         navBarImageTitle.frame = CGRect(x: 0, y: 0, width: 17, height: 17)
         navBarImageTitle.contentMode = .scaleAspectFit
-              //navBarImageTitle.center = navigationItem.titleView?.center
+        //navBarImageTitle.center = navigationItem.titleView?.center
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.isTranslucent = true
@@ -42,14 +44,5 @@ class TutorialFistViewController: UIViewController {
         
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
