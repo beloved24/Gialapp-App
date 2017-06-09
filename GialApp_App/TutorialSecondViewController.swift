88493fd4089b6f436c423b469415b9589f01369e
Andrew Amato <andrew.amato95@gmail.com>
2017-06-09 17:23:57 +0200

    ho risistemato i contraenti ed aggiunto la fase di selezione dell'auto

--- GialApp_App/TutorialSecondViewController.swift
+++ GialApp_App/TutorialSecondViewController.swift
@@ -12,7 +12,7 @@ class TutorialSecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.red
+        
         //setNavigationBar()
         // Do any additional setup after loading the view.
     }
