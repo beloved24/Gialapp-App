96a01a397e327e13fe8fade9a1a5b6ce3354b708
Andrew Amato <andrew.amato95@gmail.com>
2017-06-15 09:36:52 +0200

    non mi ricordo cosa ho fatto

--- GialApp_App/MyCustomImageView.swift
+++ GialApp_App/MyCustomImageView.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+enum imageType {
+    case big
+    case small
+    case medium
+}
 
 class MyCustomImageView: UIImageView {
 
@@ -27,7 +32,7 @@ class MyCustomImageView: UIImageView {
             let position : CGPoint = touch.location(in: self)
             let xANDy = (position.x, position.y)
             
-            //print("touched on x: \(position.x) , y: \(position.y)")
+            print("touched on x: \(position.x) , y: \(position.y)")
             
             if widthOfTheImage < 335 {
                 print("dovrebbe essere un iPhone 6s o 7 in landscape con larghezza immagine a 335")
@@ -38,45 +43,99 @@ class MyCustomImageView: UIImageView {
                 case let (x,y) where isInRange(value: Float(x), leftLimit: 131, rightLimit: 201) && isInRange(value: Float(y), leftLimit: 31, rightLimit: 46):
                     print("specchietto retrovisore")
                     touchedArea = "specchietto retrovisore"
-                    openSelectedArea(area: touchedArea)
+                    openSelectedArea(area: touchedArea, type: .small)
+                    break
                     
-                    
-                case let (x,y) where isInRange(value: Float(x), leftLimit: 0, rightLimit: 333) && isInRange(value: Float(y), leftLimit: 0, rightLimit: 100) :
+                case let (x,y) where isInRange(value: Float(x), leftLimit: 0, rightLimit: 333) && isInRange(value: Float(y), leftLimit: 0, rightLimit: 114) :
                     print("parabrezza")
                     touchedArea = "parabrezza"
-                    openSelectedArea(area: touchedArea)
+                    openSelectedArea(area: touchedArea, type: .big)
+                    break
+                    
                     
                 case let (x,y) where isInRange(value: Float(x), leftLimit: 24, rightLimit: 119) && isInRange(value: Float(y), leftLimit: 100, rightLimit: 188):
                     print("volante")
                     touchedArea = "volante"
-                    openSelectedArea(area: touchedArea)
+                    openSelectedArea(area: touchedArea, type: .big)
+                    break
                 
-                case let (x,y) where isInRange(value: Float(x), leftLimit: 120, rightLimit: 215) && isInRange(value: Float(y), leftLimit: 102, rightLimit: 187):
+                case let (x,y) where isInRange(value: Float(x), leftLimit: 120, rightLimit: 215) && isInRange(value: Float(y), leftLimit: 114, rightLimit: 187):
                     print("plancia centrale")
                     touchedArea = "plancia centrale"
-                    openSelectedArea(area: touchedArea)
+                    openSelectedArea(area: touchedArea, type: .big)
+                    break
                     
                 case let (x,y) where isInRange(value: Float(x), leftLimit: 212, rightLimit: 333) && isInRange(value: Float(y), leftLimit: 108, rightLimit: 187):
                     print("plancia destra")
                     touchedArea = "plancia destra"
-                    openSelectedArea(area: touchedArea)
+                    openSelectedArea(area: touchedArea, type: .big)
+                    break
                 
                 case let (x,y) where isInRange(value: Float(x), leftLimit: 140, rightLimit: 194) && isInRange(value: Float(y), leftLimit: 189, rightLimit: 245):
                     print("tunnel centrale")
                     touchedArea = "tunnel centrale"
-                    openSelectedArea(area: touchedArea)
+                    openSelectedArea(area: touchedArea, type: .small)
+                    break
                     
                 default:
                     print("qualcosa è andato storto")
                     print("touched on x: \(position.x), y: \(position.y) =")
+                    break
                 }
-            }
                 
+            }
+            
             else if widthOfTheImage > 335 && widthOfTheImage < 370 {
                 //iphone 6s+ e 7+ landscape
+                
             }
             else if widthOfTheImage > 370 && widthOfTheImage < 376 {
-                //iphone 6s e 6 portrait
+                //print("dovrebbe essere un iPhone 6s o 7 in portrait con larghezza immagine a 335")
+                //iphone 6s e 7 portrait
+               switch xANDy {
+                case let (x,y) where isInRange(value: Float(x), leftLimit: 148, rightLimit: 225) && isInRange(value: Float(y), leftLimit: 34, rightLimit: 50):
+                    print("specchietto retrovisore")
+                    touchedArea = "specchietto retrovisore"
+                    openSelectedArea(area: touchedArea, type: .small)
+                    break
+               
+               case let (x,y) where isInRange(value: Float(x), leftLimit: 0, rightLimit: 375) && isInRange(value: Float(y), leftLimit: 0, rightLimit: 114) :
+                print("parabrezza")
+                touchedArea = "parabrezza"
+                openSelectedArea(area: touchedArea, type: .big)
+                //qua non ci vuole il break
+                
+  
+                case let (x,y) where isInRange(value: Float(x), leftLimit: 27, rightLimit: 134) && isInRange(value: Float(y), leftLimit: 102, rightLimit: 210):
+                    print("volante")
+                    touchedArea = "volante"
+                    openSelectedArea(area: touchedArea, type: .big)
+                    break
+                    
+                
+                case let (x,y) where isInRange(value: Float(x), leftLimit: 141, rightLimit: 239) && isInRange(value: Float(y), leftLimit: 119, rightLimit: 207):
+                    print("plancia centrale")
+                    touchedArea = "plancia centrale"
+                    openSelectedArea(area: touchedArea, type: .big)
+                    break
+                    
+                case let (x,y) where isInRange(value: Float(x), leftLimit: 240, rightLimit: 375) && isInRange(value: Float(y), leftLimit: 119, rightLimit: 211):
+                    print("plancia destra")
+                    touchedArea = "plancia destra"
+                    openSelectedArea(area: touchedArea, type: .big)
+                    break
+                    
+                case let (x,y) where isInRange(value: Float(x), leftLimit: 157, rightLimit: 219) && isInRange(value: Float(y), leftLimit: 212, rightLimit: 273):
+                    print("tunnel centrale")
+                    touchedArea = "tunnel centrale"
+                    openSelectedArea(area: touchedArea, type: .small)
+                    break
+                    
+                default:
+                    print("qualcosa è andato storto")
+                    print("touched on x: \(position.x), y: \(position.y) =")
+                    break
+                }
             }
             else if widthOfTheImage > 376 && widthOfTheImage < 417 {
                 //iphone 6s+ e 7+ portrait
@@ -101,21 +160,31 @@ class MyCustomImageView: UIImageView {
         return false
     }
     
-    func openSelectedArea(area: String) {
+    func openSelectedArea(area: String, type: imageType) {
         print("open selected area")
         let viewOfTheSelectedArea = UIView()
         viewOfTheSelectedArea.frame = (superview?.frame)!
         viewOfTheSelectedArea.center = (superview?.center)!
-        viewOfTheSelectedArea.backgroundColor = UIColor.green
+        viewOfTheSelectedArea.backgroundColor = UIColor.init(red: 0.09, green: 0.39, blue: 0.49, alpha: 1.0)
         
         
         let imageArea = MyCustomImageView()
         imageArea.image = UIImage(named: area)
         imageArea.contentMode = .scaleAspectFit
-        imageArea.frame.size.width = viewOfTheSelectedArea.frame.size.width/3
+        switch type {
+        case .big:
+            imageArea.frame.size.width = viewOfTheSelectedArea.frame.size.width
+            break
+        case .medium:
+            imageArea.frame.size.width = viewOfTheSelectedArea.frame.size.width/2
+            break
+        case .small:
+            imageArea.frame.size.width = viewOfTheSelectedArea.frame.size.width/3
+            break
+        }
         imageArea.frame.size.height = imageArea.frame.size.width
         imageArea.center = viewOfTheSelectedArea.center
-        imageArea.backgroundColor = UIColor.red
+        imageArea.backgroundColor = UIColor.init(red: 0.09, green: 0.39, blue: 0.49, alpha: 1.0)
         
         let tapToclose = UITapGestureRecognizer(target: self, action: #selector(tapToCloseFunction))
         viewOfTheSelectedArea.addGestureRecognizer(tapToclose)
--- GialApp_App/TutorialThirdViewController.swift
+++ GialApp_App/TutorialThirdViewController.swift
@@ -8,11 +8,63 @@
 
 import UIKit
 
+
 class TutorialThirdViewController: UIViewController {
 
+    //MARK: outlet and properties
+    @IBOutlet weak var allViewsSV: UIStackView!
+    @IBOutlet weak var oView0: UIView!
+    @IBOutlet weak var oView1: UIView!
+    @IBOutlet weak var oView2: UIView!
+    @IBOutlet weak var oView3: UIView!
+    var origin0 = CGPoint()
+    var width0 = CGFloat()
+    var height0 = CGFloat()
+    
+    var center1 = CGPoint()
+    var center2 = CGPoint()
+    var center3 = CGPoint()
+    
+    var size0 = CGSize()
+    var size1 = CGSize()
+    var size2 = CGSize()
+    var size3 = CGSize()
+
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        origin0 = oView0.frame.origin
+        width0 = oView0.frame.width
+        height0 = oView0.frame.height
+        center1 = oView1.center
+        center2 = oView2.center
+        center3 = oView3.center
+        size0 = oView0.frame.size
+        size1 = oView1.frame.size
+        size2 = oView2.frame.size
+        size3 = oView3.frame.size
         
+        super.viewDidLoad()
+        let tapToEnlarge0 : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapToEnlargeFunction))
+        let tapToEnlarge1 : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapToEnlargeFunction))
+        let tapToEnlarge2 : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapToEnlargeFunction))
+        let tapToEnlarge3 : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapToEnlargeFunction))
+
+        allViewsSV.arrangedSubviews[0].addGestureRecognizer(tapToEnlarge0)
+        allViewsSV.arrangedSubviews[1].addGestureRecognizer(tapToEnlarge1)
+        allViewsSV.arrangedSubviews[2].addGestureRecognizer(tapToEnlarge2)
+//        allViewsSV.arrangedSubviews[3].addGestureRecognizer(tapToEnlarge3)
+//        
+//
+//        oView0.addGestureRecognizer(tapToEnlarge0)
+//        oView1.addGestureRecognizer(tapToEnlarge1)
+//        oView2.addGestureRecognizer(tapToEnlarge2)
+//        oView3.addGestureRecognizer(tapToEnlarge3)
+//        
+//        allViewsSV.arrangedSubviews[1].addGestureRecognizer(tapToEnlarge)
+//        allViewsSV.arrangedSubviews[2].addGestureRecognizer(tapToEnlarge)
+        
+      
         // Do any additional setup after loading the view.
     }
 
@@ -26,14 +78,31 @@ class TutorialThirdViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
+  
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension TutorialThirdViewController {
+    func tapToEnlargeFunction (gesture: UIGestureRecognizer) {
+        
+        self.allViewsSV.removeArrangedSubview(oView1)
+
+        UIView.animate(withDuration: 0.4) {
+            self.oView0.removeFromSuperview()
+            
+            self.allViewsSV.addArrangedSubview(self.oView0)
+            self.oView1.frame.origin = self.origin0
+            self.oView1.frame.size.width = self.width0
+            self.oView1.frame.size.height = self.height0
+            
+            self.view.addSubview(self.oView1)
+            
+            print("\ncenter1: \(self.center1)")
+            print("\ncenter2: \(self.center2)")
+            print("\ncenter3: \(self.center3)")
+
+        }
+       
+        print("inside the tap to enlarge function")
     }
-    */
-
+    
 }
