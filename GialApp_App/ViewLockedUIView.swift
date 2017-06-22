6d4d58072a1813fb156302a42305890f403208f1
Andrew Amato <andrew.amato95@gmail.com>
2017-06-22 17:14:03 +0200

    create le classi

---
+++ GialApp_App/ViewLockedUIView.swift
@@ -0,0 +1,21 @@
+//
+//  ViewLockedUIView.swift
+//  GialApp_App
+//
+//  Created by Andrew Amato on 22/06/17.
+//  Copyright © 2017 Andrew Amato. All rights reserved.
+//
+
+import UIKit
+
+class ViewLockedUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
