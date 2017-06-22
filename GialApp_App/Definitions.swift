c4d937f285128844ee91588d3147335cb5b60a7b
Andrew Amato <andrew.amato95@gmail.com>
2017-06-22 22:00:26 +0200

    ho aggiunto un po' di documentation

--- GialApp_App/Definitions.swift
+++ GialApp_App/Definitions.swift
@@ -12,3 +12,9 @@ import UIKit
 let placeholderGrey = UIColor(colorLiteralRed: 168/255, green: 182/255, blue: 197/255, alpha: 1)
 let lightBlueMegaride = UIColor(colorLiteralRed: 75/255, green: 124/255, blue: 128/255, alpha: 1)
 let blueMegaride = UIColor(colorLiteralRed: 24/255, green: 100/255, blue: 126/255, alpha: 1)
+
+enum typeOfClass: String {
+    case ViewGforceUIView = "ViewGforceUIView"
+    case ViewAccelerazioneUIView = "ViewAccelerazioneUIView"
+    case ViewRuoteUIView = "ViewRuoteUIView"
+}
--- GialApp_App/MainUIViewController.swift
+++ GialApp_App/MainUIViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import CoreMotion
 
+
+
 class MainUIViewController: UIViewController {
 
     @IBOutlet weak var view0: UIView!
@@ -23,12 +25,22 @@ class MainUIViewController: UIViewController {
     let motionManager = CMMotionManager()
     
     
+    //MARK: andrew
+    var center0 = CGPoint()
+    var center1 = CGPoint()
+    var center2 = CGPoint()
+    var center3 = CGPoint()
+    var frame0 = CGRect()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view0.addSubview(viewRuote)
-        view1.addSubview(viewGforce)
-        viewGforce.frame.size.width = view1.frame.width
-        viewGforce.frame.size.height = view1.frame.height
+        view1.addSubview(viewAccelerazione)
+        viewAccelerazione.frame.size.width = view1.frame.width
+        viewAccelerazione.frame.size.height = view1.frame.height
         
         let tapToEnlarge0 : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapToEnlargeFunction))
         let tapToEnlarge1 : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapToEnlargeFunction))
@@ -39,6 +51,13 @@ class MainUIViewController: UIViewController {
         view1.addGestureRecognizer(tapToEnlarge1)
         view2.addGestureRecognizer(tapToEnlarge2)
         view3.addGestureRecognizer(tapToEnlarge3)
+        
+        center0 = view0.center
+        center1 = view1.center
+        center2 = view2.center
+        center3 = view3.center
+
+        frame0 = view0.frame
         
 
         
@@ -54,50 +73,65 @@ class MainUIViewController: UIViewController {
 
 extension MainUIViewController {
     
+    /// praticamente questa è la funzione che abbiamo sempre usato. sposta la view tappata al centro e porta quella al centro al posto della view tappata
+    ///
+    /// - Parameter gesture: il gesture recognizer che la chiamata. lo usiamo anche per capire qual è la view che è stata toccata
     func tapToEnlargeFunction (gesture: UIGestureRecognizer) {
         //la view che sta al centro è sempre la view con tag 0
         let destinationFrame = gesture.view!.frame
         let destinationCenter = gesture.view?.center
         
         //movingBackFunction(movingView: self.view.viewWithTag(99)! as! MyCustomUIView, at: destinationCenter!, with: destinationFrame)
-        movingViewAtCenter(movingView: gesture.view! as! MyCustomUIView)
+        movingViewAtCenter(movingView: gesture.view!)
         
     }
     
+    /// questa funzione prende la view in ingresso e la sposta al centro. gli elementi contenuti all'interno delle singole view vengono gestiti dalle relative funzioni che stanno nelle classi personalizzate che abbiamo cerato
+    ///
+    /// - Parameter movingView: la moving view è la view tappata, quindi la prendiamo dal gesture recognizer. prima pero di assergnarla alle varie funzioni delle classi personalizzate, viene castata al tipo corrispondente (individuato tramite la funzione is kind of).
     func movingViewAtCenter (movingView: UIView) {
         if (movingView.subviews.first!.isKind(of: ViewRuoteUIView.self)) {
+            print("ho toccato la view delle ruote")
             let gestureView = movingView.subviews.first as! ViewRuoteUIView
             //questa gesture view è quella da allargare
+            animaAlCentro(movingView: gestureView, ofType: .ViewRuoteUIView)
         }
         else if (movingView.subviews.first!.isKind(of: ViewAccelerazioneUIView.self)) {
+            print("ho toccato la view dell'accelerazione")
+
             let gestureView = movingView.subviews.first as! ViewAccelerazioneUIView
+            animaAlCentro(movingView: gestureView, ofType: .ViewAccelerazioneUIView)
         }
         else if  (movingView.subviews.first!.isKind(of: ViewGforceUIView.self)) {
+            print("ho toccato la view dell FORZA G")
+
             let gestureView = movingView.subviews.first as! ViewGforceUIView
+            animaAlCentro(movingView: gestureView, ofType: .ViewGforceUIView)
         }
         
     }
     
-       /* if let _  = movingView.subviews.first {
-            //quì gestisco la cafonata
-            //movingView.subviews.first!.alpha = 0
-            // quella che contiente tutte le cose che non servono quando è piccola adesso deve tornare ad essere visibile
+  
+    /// per evitare ridondanza ho fatto a aprte un'altra funzione che contiente l'animazione vera e propria. essa a seconda dei vari casi, oltre ad eseguire sempre la stessa parte di codice ogni volta (che pure si poteva mettere in una funzione a parte ma non volevo esagerare con le funzioni innestate)  che mi sposta la view, esegue a seconda del tipo anche il codice che ti allarga propriamente il contenuto 
+    ///
+    /// - Parameters:
+    ///   - movingView: la view presa dal gesture recognizer, quindi la view che è stata tappata
+    ///   - ofType: questo serve per sapere quale view sto allargando in modo da pter disporre correttamente il contenuto
+    func animaAlCentro(movingView: UIView, ofType: typeOfClass) {
+        switch ofType {
+        case .ViewAccelerazioneUIView:
             UIView.animate(withDuration: 0.2, delay: 0, options: [.curveEaseInOut], animations: {
                 let center = movingView.center
                 movingView.frame.size.width *= 2
                 movingView.frame.size.height *= 2
                 movingView.center = center
-                movingView.subviews.first?.frame.size.width *= 2
-                movingView.subviews.first?.frame.size.height *= 2
+                //quì devi allargare il contenuto della view
             }, completion: {
                 if $0 {
-                    //quì gestisco lo spostamento vero e proprio
                     UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut], animations: {
                         movingView.center = self.center0
                         movingView.frame = self.frame0
-                        movingView.subviews.first?.frame = self.frameGRANDEfirstSubviewView0
-                        self.setView(movingView: movingView)
-                        movingView.subviews[1].alpha = 1 //rendo il containre visibile dopo che l'ho settato in moveview
+                        //quì devi spostare il contenuto della view
                         
                     }, completion: {
                         if $0 {
@@ -106,55 +140,21 @@ extension MainUIViewController {
                     })
                     
                 }
-            })
-        }
-            
-        else {
-            UIView.animate(withDuration: 0.2, delay: 0, options: [.curveEaseInOut], animations: {
-                let center = movingView.center
-                movingView.frame.size.width *= 2
-                movingView.frame.size.height *= 2
-                movingView.center = center
                 
-            }, completion: {
-                if $0 {
-                    UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut], animations: {
-                        movingView.center = self.center0
-                        movingView.frame = self.frame0
-                        
-                        //movingView.subviews.first?.frame = self.frameGRANDEfirstSubviewView0
-                        //movingView.subviews[1].alpha = 1
-                    }, completion: {
-                        if $0 {
-                            movingView.tag = 99
-                        }
-                    })
-                }
             })
-        }
-        movingView.isUserInteractionEnabled = false
+
+            break
+        case .ViewGforceUIView:
         
+            break
+        case .ViewRuoteUIView:
+            break
+        default:
+            print("fratè fatt a rot")
+            
+        }
+                movingView.isUserInteractionEnabled = false
     }
-    
-    
-    
-   
-     func tapToEnlargeFunction (gesture: UIGestureRecognizer)  {
-        
-        
-        if (gesture.view?.subviews.first!.isKind(of: ViewRuoteUIView.self))! {
-            let gestureView = gesture.view?.subviews.first as! ViewRuoteUIView
-            //questa gesture view è quella da allargare
-            
-            
-        }
-        else if (gesture.view?.subviews.first!.isKind(of: ViewAccelerazioneUIView.self))! {
-            
-        }
-        else if  (gesture.view?.subviews.first!.isKind(of: ViewGforceUIView.self))! {
-            
-        }
-     */
 }
 
 
